177982454f13d74fb0af3dccde4230ba3a6ee5ed
Hicham Atanane <hicham.atn@gmail.com>
2022-05-30 00:37:02 +0100

    exercice1 sans iteration au niveau des diagonals

--- src/exercice1/Main.java
+++ src/exercice1/Main.java
@@ -13,8 +13,8 @@ public class Main {
 
         AgentJoueur j1 = new AgentJoueur('X');
         AgentJoueur j2 = new AgentJoueur('O');
-        int counter = 0;
-        while (!gameOver(ticTacToe) && counter < 10){
+        int counter= 0;
+        while (!gameOver(ticTacToe) && counter< 10){
             System.out.println("------------");
             j1.jouer(ticTacToe);
             affichage(ticTacToe);
@@ -26,8 +26,8 @@ public class Main {
                 break;
             }
 
-            counter ++;
-            if (counter == 9){
+            counter++;
+            if (counter== 9){
                 System.out.println("Match nulle");
                 break;
             }
@@ -41,10 +41,10 @@ public class Main {
                 System.out.println(j2.getSymbole()+" a gagné");
                 break;
             }
-            counter ++;
+            counter++;
         }
     }
-//--------------------Methods--------------------------------------
+    //--------------------Methods--------------------------------------
     public static void affichage(Character[][] ticTacToe){
         System.out.println(" |0|1|2|");
         for (int i = 0; i < 3; i++) {
@@ -57,7 +57,7 @@ public class Main {
             System.out.println();
         }
     }
-//------------------------------------------------------------------
+    //------------------------------------------------------------------
     public static boolean gameOver(Character[][] ticTacToe){
         boolean flag = true;
         for (int i = 0; i < 3; i++) {
@@ -71,53 +71,39 @@ public class Main {
         }
         return flag;
     }
-//------------------------------------------------------------------
+    //------------------------------------------------------------------
     public static boolean gagner(Character[][] ticTacToe, char symbole){
         boolean flag = false;
         char similaire;
-        int counter1;
-        int counter2;
+        int counter;
 //  par ligne
         for (int i = 0; i < 3; i++) {
-            counter1 = 0;
+            counter = 0;
             similaire = ticTacToe[i][0];
             if (similaire!='_'){
                 for (int j = 1; j < 3; j++) {
                     if (ticTacToe[i][j]!=similaire){ break;}
-                    counter1 ++;
+                    counter ++;
                 }
             }
-            if (counter1 == 2){ flag = true; break;}
+            if (counter == 2){ flag = true; break;}
         }
 //  par colonne
         for (int i = 0; i < 3; i++) {
-            counter1 = 0;
+            counter = 0;
             similaire = ticTacToe[0][i];
             if (similaire!='_'){
                 for (int j = 1; j < 3; j++) {
                     if (ticTacToe[j][i]!=similaire){ break;}
-                    counter1 ++;
+                    counter ++;
                 }
             }
-            if (counter1 == 2){ flag = true; break;}
+            if (counter == 2){ flag = true; break;}
         }
 //  par diagonale
-        for (int i = 0; i < 3; i++) {
-            similaire = ticTacToe[i][i];
-            counter1 = 0;
-            counter2 = 0;
-            if (similaire!='_'){
-                for (int j = 1; j < 3; j++) {
-                    if (ticTacToe[j][j]!=similaire){ break;}
-                    counter1 ++;
-                }
-                for (int j = 1; j < 3; j++) {
-                    if (ticTacToe[j][2-j]!=similaire){ break;}
-                    counter2 ++;
-                }
-            }
-            if (counter1 == 2 || counter2 == 2){ flag = true; break;}
-        }
+        if (ticTacToe[1][1]!='_' && ticTacToe[2][0]==ticTacToe[1][1] && ticTacToe[1][1]==ticTacToe[0][2]){flag=true;}
+        else if (ticTacToe[1][1]!='_' && ticTacToe[0][0]==ticTacToe[1][1] && ticTacToe[1][1]==ticTacToe[2][2]){flag=true;}
+
         return flag;
     }
 }
